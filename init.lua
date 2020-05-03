cb4c1808e37d9b7ba79708b30667be3fb48c62c0
dkok97 <dinkarkhattar@gmail.com>
2020-05-02 21:38:07 -0700

    move mouse

--- init.lua
+++ init.lua
@@ -12,6 +12,10 @@ end
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 
-hs.hotkey.bind({}, "F", function()
-    hs.osascript.applescriptFromFile('addtocart.applescript')
+hs.hotkey.bind({"cmd", "alt"}, "F", function()
+    -- hs.osascript.applescriptFromFile('addtocart.applescript')
+    -- pos = hs.mouse.getAbsolutePosition()
+    -- print(pos.x)
+    -- print(pos.y)
+    hs.mouse.setAbsolutePosition({x=1003.6796875,y=615.08984375})
 end)
